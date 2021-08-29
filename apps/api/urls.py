7fe494955692d102eb00d1ab76fd725e9bc1332b
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2021-08-29 23:49:49 +0900

    TokenObtainPairView 문서화 오류 수정

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -1,14 +1,25 @@
 from apps.user.views import UserViewSet
 from django.urls import path
+from rest_framework import serializers
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import (TokenObtainPairView,
                                             TokenRefreshView, TokenVerifyView)
 
 router = DefaultRouter(trailing_slash=False)
 router.register(r'users', UserViewSet, basename='users')
 
+
+class DocumentedTokenObtainPairView(TokenObtainPairView):
+    class DocumentedTokenObtainPairSerializer(TokenObtainPairSerializer):
+        refresh = serializers.CharField(read_only=True)
+        access = serializers.CharField(read_only=True)
+
+    serializer_class = DocumentedTokenObtainPairSerializer
+
+
 urlpatterns = [
-    path('token', TokenObtainPairView.as_view()),
+    path('token', DocumentedTokenObtainPairView.as_view()),
     path('token/refresh', TokenRefreshView.as_view()),
     path('token/verify', TokenVerifyView.as_view())
 ]
