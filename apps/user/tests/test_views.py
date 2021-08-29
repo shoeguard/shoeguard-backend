12de5f9e03c0884c2253891a2b6a78923ecf221e
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2021-08-29 22:39:35 +0900

    phone_number 가 없을 경우 예외 처리 추가

--- apps/user/tests/test_views.py
+++ apps/user/tests/test_views.py
@@ -2,8 +2,13 @@ import pytest
 from django.test.client import Client
 
 
+@pytest.mark.django_db(transaction=True)
 def test_fail_when_no_phone_number(client: Client):
-    pass
+    response = client.post(
+        '/api/v1/users/register',
+        data={"password": "e10nMuskP@ssword"},
+    )
+    assert response.status_code == 400
 
 
 def test_fail_when_no_phone_number(client: Client):
--- apps/user/views.py
+++ apps/user/views.py
@@ -7,4 +7,10 @@ from apps.user.serializer import UserSerializer
 
 
 class UserViewSet(viewsets.GenericViewSet):
-    pass
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+    @action(methods=['post'], detail=False)
+    def register(self, request: HttpRequest, *args, **kwargs):
+        serializer: UserSerializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
