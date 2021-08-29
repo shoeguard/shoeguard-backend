6da834878e84e363712e0131c17e53de4baa82d6
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2021-08-29 19:53:33 +0900

    User 모델 추가

--- apps/user/admin.py
+++ apps/user/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.user.models import ParentChildPair, User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'phone_number', 'name', 'is_child')
+
+
+admin.site.register(ParentChildPair)
--- apps/user/models.py
+++ apps/user/models.py
@@ -1,3 +1,90 @@
+from typing import Union
+
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import BaseUserManager, PermissionsMixin
 from django.db import models
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def create_user(
+        self,
+        phone_number,
+        name,
+        password=None,
+    ):
+        """
+        Creates and saves a User with the given phone_number, name and password.
+        """
+        if not phone_number:
+            raise ValueError('Users must have an phone_number')
+        user = self.model(
+            phone_number=phone_number,
+            name=name,
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, phone_number, name, password):
+        """
+        Creates and saves a superuser with the given phone_number, name and password.
+        """
+        user: User = self.create_user(
+            phone_number=phone_number,
+            name=name,
+        )
+        user.is_verified = True
+        user.is_superuser = True
+        user.is_staff = True
+        user.is_admin = True
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+
+class ParentChildPair(models.Model):
+    child = models.ForeignKey(
+        'User',
+        on_delete=models.CASCADE,
+        related_name='child',
+    )
+    parent = models.ForeignKey(
+        'User',
+        on_delete=models.CASCADE,
+        related_name='parent',
+    )
+
+    def save(self, *args, **kwargs):
+        if self.parent == self.child:
+            raise ValueError('Parent and Child must not be the same')
+        super(ParentChildPair, self).save(*args, **kwargs)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    objects = UserManager()
+
+    USERNAME_FIELD = 'phone_number'
+    REQUIRED_FIELDS = ['name']
+
+    phone_number = models.CharField(max_length=12, unique=True)
+    name = models.CharField(max_length=4)
+    partner = models.ForeignKey(
+        ParentChildPair,
+        related_name='partner',
+        on_delete=models.PROTECT,
+        null=True,
+    )
+
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+
+    @property
+    def is_child(self) -> Union[bool, None]:
+        if self.partner is None:
+            return None
+        return self.partner.child == self
+
+    def __str__(self) -> str:
+        return self.name
