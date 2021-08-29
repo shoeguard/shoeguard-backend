13dea0fc5bae2e17d95a987668a1274ef93e245c
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2021-08-29 23:51:16 +0900

    인증되지 않은 사용자가 요청 했을 경우 예외 처리 추가

--- apps/user/tests/test_get_me.py
+++ apps/user/tests/test_get_me.py
@@ -1,10 +1,19 @@
 import pytest
+from apps.user.models import User
 from django.test.client import Client
 
+ENDPOINT = '/api/v1/users/me'
 
+
+@pytest.mark.django_db(transaction=True)
 def test_fail_when_not_authenticated(client: Client):
-    pass
+    # when
+    response = client.get(ENDPOINT)
+
+    # then
+    assert response.status_code == 401
 
 
+@pytest.mark.django_db(transaction=True)
 def test_success(client: Client):
     pass
--- apps/user/views.py
+++ apps/user/views.py
@@ -1,5 +1,6 @@
 from django.http.request import HttpRequest
-from rest_framework import status, viewsets
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework import permissions, status, viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
@@ -10,6 +11,11 @@ from apps.user.serializer import UserSerializer
 class UserViewSet(viewsets.GenericViewSet):
     serializer_class = UserSerializer
     queryset = User.objects.all()
+
+    def get_permissions(self):
+        if self.action == 'me':
+            return (permissions.IsAuthenticated(), )
+        return (permissions.AllowAny(), )
 
     @action(methods=['post'], detail=False)
     def register(self, request: HttpRequest, *args, **kwargs):
