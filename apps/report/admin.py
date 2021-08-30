19da8cc712dc430c6aa2f75ea5db887e24bdc8eb
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2021-08-30 14:46:46 +0900

    admin 에 Report 모델 등록

--- apps/report/admin.py
+++ apps/report/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from django_restful_admin import admin as api_admin
 
-# Register your models here.
+from apps.report.models import Report
+
+admin.site.register(Report)
+api_admin.site.register(Report)
