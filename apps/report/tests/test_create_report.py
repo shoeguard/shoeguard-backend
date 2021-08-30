4fd0a1fbb4518972b2e0a70dfb35c54bb6f4bde8
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2021-08-30 19:48:36 +0900

    json Content-Type 으로 요청을 보내도록 변경

--- apps/report/tests/test_create_report.py
+++ apps/report/tests/test_create_report.py
@@ -12,7 +12,11 @@ DATA = {"address": "서울시 용산구 원효로97길 33-4", "reported_device":
 @pytest.mark.django_db(transaction=True)
 def test_fail_when_not_authenticated(client: Client):
     # when
-    response = client.post(ENDPOINT, data=DATA)
+    response = client.post(
+        ENDPOINT,
+        json.dumps(DATA),
+        content_type="application/json",
+    )
 
     # then
     assert response.status_code == 401
@@ -31,6 +35,7 @@ def test_fail_when_requested_user_has_no_parent_child_pair(
     response = client.post(
         ENDPOINT,
         json.dumps(DATA),
+        content_type="application/json",
         HTTP_AUTHORIZATION=f"Bearer {token}",
     )
 
