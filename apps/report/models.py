c39290b69ad8b94c1b6ee2b01c3a44faefc15a3e
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2021-08-30 14:43:13 +0900

    Report 모델 추가

--- apps/report/models.py
+++ apps/report/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+from apps.common.models import BaseModel
+from apps.user.models import ParentChildPair
+
+
+class Report(BaseModel):
+    class ReportedDeviceType(models.TextChoices):
+        IOT = 'IOT', _('IOT')
+        PHONE = 'PHONE', _('PHONE')
+
+    parent_child_pair = models.ForeignKey(
+        ParentChildPair,
+        on_delete=models.CASCADE,
+    )
+    address = models.CharField(max_length=255)
+    latitude = models.FloatField(null=True)
+    longitude = models.FloatField(null=True)
+    audio_url = models.URLField(null=True)
+    reported_device = models.CharField(
+        max_length=5,
+        choices=ReportedDeviceType.choices,
+    )
