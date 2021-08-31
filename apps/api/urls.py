531ade166ef685564cdaef22a10fb14a0137f9da
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2021-08-31 22:28:31 +0900

    drf_spectacular 에서 기본 제공 되는 문서로 token 관련 내용 교체

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -2,9 +2,7 @@ from apps.location_history.views import LocationHistoryViewSet
 from apps.report.views import ReportViewSet
 from apps.user.views import ParentChildPairViewSet, UserViewSet
 from django.urls import path
-from rest_framework import serializers
 from rest_framework.routers import DefaultRouter
-from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import (TokenObtainPairView,
                                             TokenRefreshView, TokenVerifyView)
 
@@ -23,16 +21,9 @@ router.register(
 )
 
 
-class DocumentedTokenObtainPairView(TokenObtainPairView):
-    class DocumentedTokenObtainPairSerializer(TokenObtainPairSerializer):
-        refresh = serializers.CharField(read_only=True)
-        access = serializers.CharField(read_only=True)
-
-    serializer_class = DocumentedTokenObtainPairSerializer
-
 
 urlpatterns = [
-    path('token', DocumentedTokenObtainPairView.as_view()),
+    path('token', TokenObtainPairView.as_view()),
     path('token/refresh', TokenRefreshView.as_view()),
     path('token/verify', TokenVerifyView.as_view())
 ]
