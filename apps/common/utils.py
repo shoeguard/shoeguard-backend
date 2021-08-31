31186d4179482bfa1853a25846e524cd82d18e1e
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2021-08-31 11:15:06 +0900

    geocoding 과 관련한 util 추가

---
+++ apps/common/utils.py
@@ -0,0 +1,18 @@
+from geopy.geocoders import Nominatim
+
+geolocator = Nominatim(user_agent="shoeguard")
+from typing import Union
+
+
+def geocode(address: str) -> Union[tuple[float, float], None]:
+    location = geolocator.geocode(address)
+    if location is None:
+        return None
+    return (location.latitude, location.longitude)
+
+
+def reverse_geocode(latitude: float, longitude: float) -> Union[str, None]:
+    location = geolocator.reverse((latitude, longitude))
+    if location is None:
+        return None
+    return location.address
