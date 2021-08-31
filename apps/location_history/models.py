a052b43db492584a6eb13f7953643cb15fb8b2c5
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2021-08-31 10:26:50 +0900

    LocationHistory 모델 추가

--- apps/location_history/models.py
+++ apps/location_history/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+from apps.common.models import BaseModel
+from apps.user.models import ParentChildPair
+
+
+class LocationHistory(BaseModel):
+    parent_child_pair = models.ForeignKey(
+        ParentChildPair,
+        on_delete=models.CASCADE,
+    )
+    address = models.CharField(max_length=255, null=True)
+    latitude = models.FloatField(null=True)
+    longitude = models.FloatField(null=True)
