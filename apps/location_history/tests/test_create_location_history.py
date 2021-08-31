a97248fbeb3c0eaeeb2653997d9d0f8a4a7760b9
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2021-08-31 10:47:34 +0900

    로그인 하지 않은 사용자가 LocationHistory 를 요청하는 경우 예외 처리 추가

--- apps/location_history/tests/test_create_location_history.py
+++ apps/location_history/tests/test_create_location_history.py
@@ -1,4 +1,5 @@
 import pytest
+from django.test.client import Client
 
 ENDPOINT = '/api/v1/location-history'
 DATA = {
@@ -9,8 +10,12 @@ DATA = {
 
 
 @pytest.mark.django_db(transaction=True)
-def test_fail_when_not_authenticated():
-    pass
+def test_fail_when_not_authenticated(client: Client):
+    # when
+    response = client.post(ENDPOINT)
+
+    # then
+    assert response.status_code == 401
 
 
 @pytest.mark.django_db(transaction=True)
--- apps/location_history/views.py
+++ apps/location_history/views.py
@@ -1,8 +1,13 @@
-from rest_framework import mixins, viewsets
+from rest_framework import mixins, permissions, viewsets
+
+from apps.location_history.models import LocationHistory
+from apps.location_history.serializers import LocationHistorySerializer
 
 
 class LocationHistoryViewSet(
         mixins.CreateModelMixin,
         viewsets.GenericViewSet,
 ):
-    pass
+    serializer_class = LocationHistorySerializer
+    queryset = LocationHistory.objects.all()
+    permission_classes = (permissions.IsAuthenticated, )
