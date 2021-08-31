5563b7969db26cc178b4d415ddb9effa0ad93229
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2021-08-31 12:01:43 +0900

    admin 에 LocationHistory 추가

--- apps/location_history/admin.py
+++ apps/location_history/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from django_restful_admin import admin as api_admin
 
-# Register your models here.
+from apps.location_history.models import LocationHistory
+
+admin.site.register(LocationHistory)
+api_admin.site.register(LocationHistory)
