3b8bc164707af67b131cfca3fa66d647bd6be9ed
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2021-09-24 01:08:32 +0900

    필요없는 import 제거

--- apps/location_history/views.py
+++ apps/location_history/views.py
@@ -1,8 +1,6 @@
 from typing import List
 
-from drf_spectacular.utils import extend_schema
 from rest_framework import mixins, permissions, serializers, status, viewsets
-from rest_framework.decorators import action
 from rest_framework.request import Request
 from rest_framework.response import Response
 
