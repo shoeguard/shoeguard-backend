d7e751102874eb3b335a3dcf3280325b266ba1b1
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2021-09-23 20:56:26 +0900

    Report 모델에 reporter 추가

--- apps/report/models.py
+++ apps/report/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 from apps.common.models import BaseModel
-from apps.user.models import ParentChildPair
+from apps.user.models import ParentChildPair, User
 
 
 class Report(BaseModel):
@@ -13,7 +13,9 @@ class Report(BaseModel):
     parent_child_pair = models.ForeignKey(
         ParentChildPair,
         on_delete=models.CASCADE,
+        null=True,
     )
+    reporter = models.ForeignKey(User, on_delete=models.CASCADE)
     address = models.CharField(max_length=255, null=True)
     latitude = models.FloatField(null=True)
     longitude = models.FloatField(null=True)
