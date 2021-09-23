4022ce617ea8d3ec2ed75fb0bf847559a9b128a7
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2021-09-23 21:50:30 +0900

    LocationHistory 에 reporter 필드 추가

--- apps/location_history/models.py
+++ apps/location_history/models.py
@@ -1,14 +1,16 @@
 from django.db import models
 
 from apps.common.models import BaseModel
-from apps.user.models import ParentChildPair
+from apps.user.models import ParentChildPair, User
 
 
 class LocationHistory(BaseModel):
     parent_child_pair = models.ForeignKey(
         ParentChildPair,
         on_delete=models.CASCADE,
+        null=True,
     )
+    reporter = models.ForeignKey(User, on_delete=models.CASCADE)
     address = models.CharField(max_length=255, null=True)
     latitude = models.FloatField(null=True)
     longitude = models.FloatField(null=True)
