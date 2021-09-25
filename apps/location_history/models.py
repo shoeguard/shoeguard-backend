d0dcc03c953507a2b7a65d6601166db08baad5f2
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2021-09-25 14:52:36 +0900

    LocationHistory 의 nullable 이 blankable 하도록 변경

--- apps/location_history/models.py
+++ apps/location_history/models.py
@@ -6,6 +6,6 @@ from apps.user.models import User
 
 class LocationHistory(BaseModel):
     reporter = models.ForeignKey(User, on_delete=models.CASCADE)
-    address = models.CharField(max_length=255, null=True)
-    latitude = models.FloatField(null=True)
-    longitude = models.FloatField(null=True)
+    address = models.CharField(max_length=255, blank=True, null=True)
+    latitude = models.FloatField(blank=True, null=True)
+    longitude = models.FloatField(blank=True, null=True)
