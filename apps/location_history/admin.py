204081e2bc5dd7311028741fd8a19a39825d48c8
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2021-09-25 12:18:28 +0900

    LocationHistory 어드민 개선

--- apps/location_history/admin.py
+++ apps/location_history/admin.py
@@ -3,5 +3,14 @@ from django_restful_admin import admin as api_admin
 
 from apps.location_history.models import LocationHistory
 
-admin.site.register(LocationHistory)
+
+@admin.register(LocationHistory)
+class LocationHistoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'reporter',
+        'address',
+    )
+
+
 api_admin.site.register(LocationHistory)
