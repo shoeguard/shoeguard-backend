1c8b26c57b6847245b054fb433ee657561d9ca0d
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2021-09-25 12:18:13 +0900

    Report 어드민 개선

--- apps/report/admin.py
+++ apps/report/admin.py
@@ -3,5 +3,16 @@ from django_restful_admin import admin as api_admin
 
 from apps.report.models import Report
 
-admin.site.register(Report)
+
+@admin.register(Report)
+class ReportAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'reporter',
+        'address',
+        'reported_device',
+        'is_done',
+    )
+
+
 api_admin.site.register(Report)
